29e7844b154c31766791842abb1253a398bc21a9
juliocsilvestre <jcss.silvestre@gmail.com>
2025-08-09 01:59:35 +0000

    first page and deploy

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div>
+      <h1>"Cada linha de código que escrevo é uma vitória. O que antes parecia impossível, hoje é parte da minha rotina. Estou crescendo, aprendendo e me tornando o desenvolvedor que sempre quis ser."</h1>
+    </div>
+  );
+}
+
+export default Home;
