99ab88954480f5b8376ca727d60224488a27a916
juliocsilvestre <jcss.silvestre@gmail.com>
2025-08-16 15:13:53 +0000

    adjusting text

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ function Home() {
   return (
     <div>
       <h1>"Cada linha de código que escrevo é uma vitória. O que antes parecia impossível, hoje é parte da minha rotina. Estou crescendo, aprendendo e me tornando o desenvolvedor que sempre quis ser."</h1>
+      <p>Julio Silvestre dev</p>
     </div>
   );
 }
